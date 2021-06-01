d70afc25aae9ba53b6e60ddf60eab6e1ad7b56f9
doraneko94 <shuntaro94@gmail.com>
2021-06-01 22:07:51 +0900

    usize -> rand<f64>

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,16 +2,19 @@
 
 extern crate test;
 use ndarray::*;
+use rand::distributions::Uniform;
 use option_bench::*;
 
 #[bench]
 fn raw_bench(b: &mut test::Bencher) {
+    let ud = Uniform::new::<f64, f64>(0.0, 1.0);
     let mut mat = Array2::zeros((1000, 1000));
-    b.iter(|| raw(&mut mat))
+    b.iter(|| raw(&mut mat, ud))
 }
 
 #[bench]
 fn option_bench(b: &mut test::Bencher) {
+    let ud = Uniform::new::<f64, f64>(0.0, 1.0);
     let mut op_mat = Some(Array2::zeros((1000, 1000)));
-    b.iter(|| option(&mut op_mat))
+    b.iter(|| option(&mut op_mat, ud))
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,21 @@
 use ndarray::*;
+use rand::distributions::{Uniform, Distribution};
 
 /// Calculation of Matrix
-pub fn raw(mat: &mut Array2<usize>) {
-    Zip::indexed(mat)
-        .for_each(|(i, j), a| {
-            *a += i + j;
+pub fn raw(mat: &mut Array2<f64>, ud: Uniform<f64>) {
+    Zip::from(mat)
+        .for_each(|a| {
+            *a = ud.sample(&mut rand::thread_rng());
         });
 }
 
 /// Calculation of Option<Matrix>
-pub fn option(op_mat: &mut Option<Array2<usize>>) {
+pub fn option(op_mat: &mut Option<Array2<f64>>, ud: Uniform<f64>) {
     match op_mat.as_mut() {
         Some(mat) => {
-            Zip::indexed(mat)
-                .for_each(|(i, j), a| {
-                    *a += i + j;
+            Zip::from(mat)
+                .for_each(|a| {
+                    *a = ud.sample(&mut rand::thread_rng());
                 });
         }
         None => {}
